220bb5c09f4fe025b6c7518752c7fc5d399eef22
Bijita <mebijita@gmail.com>
2026-07-05 01:27:15 +0545

    Added Initial setup

---
+++ backend/core/__init__.py
@@ -0,0 +1,57 @@
+import os
+import json
+
+from flask import Flask
+from config import config
+from flask_cors import CORS
+from flasgger import Swagger
+from dotenv import load_dotenv
+from flask_migrate import Migrate
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+load_dotenv(os.environ.get('env_file'))
+
+# flask extensions
+cors_origins = json.loads(os.environ.get('CORS_ORIGINS'))
+cors = CORS(origins=cors_origins)
+db = SQLAlchemy()
+socketio = SocketIO(
+    cors_allowed_origins=cors_origins
+)
+swagger = Swagger()
+migrate = Migrate(compare_type=True)
+marshmallow = Marshmallow()
+
+
+def create_app(config_name, **kwargs):
+    app = Flask(
+        __name__
+    )
+    app.config.from_object(config[config_name])
+
+    # Initializing flask extensions
+    db.init_app(app)
+    cors.init_app(app)
+    socketio.init_app(app)
+    migrate.init_app(app, db)
+    marshmallow.init_app(app)
+    swagger.init_app(app)
+
+    # Registering Blueprints
+    from core.process_app import bp as p_bp
+
+    app.register_blueprint(p_bp)
+
+
+    # Deleting existing chat and task sessions on reload
+    # from core.utils import delete_existing_sessions
+    # with app.app_context():
+    #     delete_existing_sessions()
+
+    return app
+
+
+# Importing models here so that they are registered with SQLAlchemy
+from core import models  # noqa: F401,E402
